d9daa4ccc73c1801f067942919b96aca16d5e890
Gaurav R <gaurav.r@innovalabs.tech>
2025-01-08 18:12:08 +0530

    group the code for room_channel

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from socketio import AsyncServer
 from socketio.asgi import ASGIApp
 from fastapi.responses import HTMLResponse
-
+from app.room import sio
 app = FastAPI()
 
-sio = AsyncServer(cors_allowed_origins="*", async_mode="asgi")
-sio_app = ASGIApp(sio)
-app.mount("/socket.io", sio_app)
+app.mount("/socket.io", ASGIApp(sio))
 
 @app.get("/")
 def read_root():
@@ -19,60 +17,3 @@ ROOM_MESSAGES = {}
 def read_room(room_id: str):
     with open("ui/room.html") as f:
         return HTMLResponse(content=f.read())
-
-@sio.on("connect", namespace="/room")
-async def connect(sid, environ):
-    print(f"-->>>> Client connected: {sid}")
-
-@sio.on("join_room", namespace="/room")
-async def join_room(sid, data):
-    room_id = data.get("room_id")
-    if not room_id:
-        return
-    
-    await sio.enter_room(sid, room_id, namespace="/room")
-    print(f"-->>>> {sid} joined room {room_id}")
-    if room_id not in ROOM_MESSAGES:
-        ROOM_MESSAGES[room_id] = []
-
-    await sio.emit("message", {"msg": f"Welcome to Room {room_id}", "messages": ROOM_MESSAGES[room_id]}, room=room_id, namespace="/room")
-
-@sio.on("disconnect", namespace="/room")
-async def disconnect(sid):
-    print(f"<<<---- Client Disconnected from This id :{sid}")
-
-@sio.on("message", namespace="/room")
-async def handle_message(sid, data):
-    room_id = data.get("room_id")
-    message = data.get("message")
-    
-    if room_id not in ROOM_MESSAGES:
-        ROOM_MESSAGES[room_id] = []
-    ROOM_MESSAGES[room_id].append(message)
-    print(f"Room {room_id} - New message: {message}")
-    await sio.emit("message", {"room_id": room_id, "messages": ROOM_MESSAGES[room_id]}, room=room_id, namespace="/room")
-
-@sio.on("delete_message", namespace="/room")
-async def delete_message(sid, data):
-    room_id = data.get("room_id")
-    message_index = data.get("message_index")
-
-    if room_id in ROOM_MESSAGES and 0 <= message_index < len(ROOM_MESSAGES[room_id]):
-        del ROOM_MESSAGES[room_id][message_index]
-        print(f"Room {room_id} - Message {message_index} deleted")
-        await sio.emit("message", {"room_id": room_id, "messages": ROOM_MESSAGES[room_id]}, room=room_id, namespace="/room")
-                   
-@sio.on("connect")
-async def connect(sid, environ):
-    print("-->>>>  New Client Connected to This id :"+" "+str(sid))
-    await sio.emit("send_msg", "Hello from Server")
-    
-@sio.on("disconnect")
-async def disconnect(sid):
-    print("--<<<< Client Disconnected from This id :"+" "+str(sid))
-    # @sio.on("message")
-
-@sio.on("message") ## recive data from client
-async def message(sid, data):
-    print(f"--{sid}->>>>>"+data)
-    await sio.emit("message", data, room=sid) ## send to all user's
---
+++ app/room.py
@@ -0,0 +1,70 @@
+from socketio import AsyncServer, AsyncNamespace
+
+class RoomNamespace(AsyncNamespace):
+    def __init__(self, namespace=None):
+        super().__init__(namespace)
+        self.room_messages = {}
+
+    async def on_connect(self, sid, environ):
+        print(f"-->>>> Client connected: {sid}")
+
+    async def on_join_room(self, sid, data):
+        room_id = data.get("room_id")
+        if not room_id:
+            return
+        
+        await self.enter_room(sid, room_id)
+        print(f"-->>>> {sid} joined room {room_id}")
+
+        # Initialize room messages if not already present
+        if room_id not in self.room_messages:
+            self.room_messages[room_id] = []
+
+        # Send welcome message and existing messages to the client
+        await self.emit("message", {
+            "msg": f"Welcome to Room {room_id}",
+            "messages": self.room_messages[room_id]
+        }, to=sid)
+
+    async def on_message(self, sid, data):
+        room_id = data.get("room_id")
+        message = data.get("message")
+
+        if not room_id or not message:
+            return
+
+        # Append the message to the room's message list
+        if room_id not in self.room_messages:
+            self.room_messages[room_id] = []
+
+        self.room_messages[room_id].append(message)
+        print(f"Room {room_id} - New message: {message}")
+
+        # Broadcast updated messages to all clients in the room
+        await self.emit("message", {
+            "room_id": room_id,
+            "messages": self.room_messages[room_id]
+        }, room=room_id)
+
+    async def on_delete_message(self, sid, data):
+        room_id = data.get("room_id")
+        message_index = data.get("message_index")
+
+        if room_id in self.room_messages and 0 <= message_index < len(self.room_messages[room_id]):
+            del self.room_messages[room_id][message_index]
+            print(f"Room {room_id} - Message {message_index} deleted")
+
+            # Broadcast updated messages to all clients in the room
+            await self.emit("message", {
+                "room_id": room_id,
+                "messages": self.room_messages[room_id]
+            }, room=room_id)
+
+    async def on_disconnect(self, sid):
+        print(f"--<<<< Client disconnected: {sid}")
+
+# Create the Socket.IO server
+sio = AsyncServer(cors_allowed_origins="*", async_mode="asgi")
+
+# Register the class-based namespace
+sio.register_namespace(RoomNamespace("/room"))
