a0ca54a2c2d90275fce7281c29ebd19b4a98ac1e
Gaurav R <GauravRewaliya@gmail.com>
2025-01-09 15:52:29 +0530

    custom socket.io documentation for FastApi

--- app/main.py
+++ app/main.py
@@ -17,3 +17,122 @@ ROOM_MESSAGES = {}
 def read_room(room_id: str):
     with open("ui/room.html") as f:
         return HTMLResponse(content=f.read())
+
+@app.get("/socket_docs/api")
+async def get_socket_actions():
+    # todo: add this dynamically if possible , or seprated doc logic
+    return [
+        {
+            "namespace": "/room",
+            "actions": [
+            {
+                "name": "connect",
+                "description": "Connect to the server",
+                "params": [],
+                "responses": []
+            },
+            {
+                "name": "disconnect",
+                "description": "Disconnect from the server",
+                "params": [],
+                "responses": []
+            },
+            {
+                "name": "join_room",
+                "description": "Join a specific room",
+                "params": [
+                { "name": "room_id", "type": "string", "example": "1234" }
+                ],
+                "responses": [
+                {
+                    "namespace": "/room",
+                    "room": "1234",
+                    "action": "join_room",
+                    "data": { "status": "success", "message": "Joined room 1234" }
+                }
+                ]
+            },
+            {
+                "name": "message",
+                "description": "Send a message to a room",
+                "params": [
+                { "name": "room_id", "type": "string", "example": "1234" },
+                { "name": "message", "type": "string", "example": "Hello, world!" }
+                ],
+                "responses": [
+                {
+                    "namespace": "/room",
+                    "room": "1234",
+                    "action": "message",
+                    "data": { "status": "success", "message": "Message sent to room 1234" }
+                }
+                ]
+            },
+            {
+                "name": "delete_message",
+                "description": "Delete a message from a room",
+                "params": [
+                { "name": "room_id", "type": "string", "example": "1234" },
+                { "name": "message_id", "type": "string", "example": "5678" }
+                ],
+                "responses": [
+                # {
+                #     "namespace": "/room",
+                #     "room": "1234",
+                #     "action": "delete_message",
+                #     "data": { "status": "success", "message": "Message deleted from room 1234" }
+                # }
+                {
+                    "namespace": "/room",
+                    "room": "1234",
+                    "action": "message",
+                    "data": { "room_id": "1234", "messages": ["all messages accept deletedd"] }
+                }
+                ]
+            }
+            ]
+        },
+        {
+            "namespace": "/notification",
+            "actions": [
+                {
+                    "name": "connect",
+                    "description": "Connect to the server",
+                    "params": [],
+                    "responses": [
+                        {
+                            "namespace": "/notification",
+                            "room": None,
+                            "skip_sid": "self_id",
+                            "action": "notification",
+                            "data": {"message": "User: sid is connected"}
+                        }
+                    ]
+                },
+                {
+                    "name": "register",
+                    "description": "Register for notifications",
+                    "params": [
+                    { "name": "user_id", "type": "string", "example": "5678" }
+                    ],
+                    "responses": [
+                    {
+                        "namespace": "/notification",
+                        "action": "register",
+                        "data": { "status": "success", "message": "Registered for notifications" }
+                    }
+                    ]
+                },
+                {
+                    "name": "disconnect",
+                    "description": "Disconnect from the server",
+                    "params": [],
+                    "responses": []
+                }
+            ]
+        }
+    ]
+@app.get("/socket_docs")
+def socket_docs():
+    with open("ui/socket_docs.html") as f:
+        return HTMLResponse(content=f.read())
